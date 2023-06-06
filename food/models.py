7c2a88f11f362a792ca2b7b9dab26119e8a37f85
Endriw <endriwbatistela@gmail.com>
2023-06-06 09:05:57 -0300

    added db and added app

--- food/models.py
+++ food/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model): # models.model is inherited
+    item_name = models.CharField(max_length=200, blank=False)   # item memory space
+    item_desc = models.CharField(max_length=200, blank=False)   # description of the item
+    item_price = models.IntegerField(blank=False)               # price of the item
+
+    def __str__(self):
+        return self.name
